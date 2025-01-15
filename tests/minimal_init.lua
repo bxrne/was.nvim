58f53eabb3573f32b64354ed273b78ac9ce7a6b6
Adam Byrne <adam.r.byrne@gmail.com>
2025-01-15 21:01:26 +0000

    check for plenary

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,2 +1,8 @@
-vim.cmd [[set runtimepath+=.]]
-vim.cmd [[set runtimepath+=~/.local/share/nvim/site/pack/packer/start/plenary.nvim]]
+local plenary_status, plenary = pcall(require, "plenary")
+if not plenary_status then
+	print("Plenary plugin not loaded")
+	return
+end
+
+vim.cmd([[set runtimepath+=.]])
+vim.cmd([[set runtimepath+=~/.local/share/nvim/site/pack/packer/start/plenary.nvim]])
